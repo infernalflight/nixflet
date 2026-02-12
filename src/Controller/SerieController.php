c5775b5430f2d0a37af868ad643e47964108bff0
fred <fred@eni.com>
2026-02-12 16:52:45 +0100

    Utilisation de la méthode findBy() avec pagination

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -6,6 +6,7 @@ use App\Entity\Serie;
 use App\Repository\SerieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -30,13 +31,41 @@ final class SerieController extends AbstractController
         return new Response('Derrick est à la maison');
     }
 
-    #[Route('/liste', name: '_liste', methods: ['GET'])]
-    public function liste(SerieRepository $serieRepository): Response
+    #[Route('/liste/{page}', name: '_liste', requirements:['page' => '\d+'], methods: ['GET'])]
+    public function liste(SerieRepository $serieRepository, ParameterBagInterface $parameterBag, int $page = 1): Response
     {
-        $series = $serieRepository->findAll();
+        # appel aux parameters définis dans le fichier config/services.yaml
+        $limit = $parameterBag->get('nb_limit_series');
+
+        # Méthode heritée du Repository
+        //$series = $serieRepository->findAll();
+
+        # restriction à page > 1
+        $page = max($page, 1);
+        $offset = ($page - 1) * $limit;
+
+        $criterias = [
+            'status' => 'returning',
+            'genres' => 'Horror'
+        ];
+
+        $nbTotal = $serieRepository->count($criterias);
+        $nbPagesMax = ceil($nbTotal / $limit);
+
+        if ($page > $nbPagesMax) {
+            throw $this->createNotFoundException('La page ' . $page . ' n\'existe pas');
+        }
+
+        # Méthode héritée qui utilise un tableau de critères binaires
+        $series = $serieRepository->findBy($criterias, [
+            'firstAirDate' => 'DESC',
+            'dateCreated' => 'DESC'
+        ], $limit, $offset);
 
         return $this->render('serie/liste.html.twig', [
             'series' => $series,
+            'page' => $page,
+            'nb_pages_max' => $nbPagesMax,
         ]);
     }
 
